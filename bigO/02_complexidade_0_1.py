40ee9a24534d5a94a8bba3659b9cb7b36b8a4372
leoCardosoDev <leocardosodev@gmail.com>
2023-11-20 15:51:29 -0300

    feat: add func with 0(1)

---
+++ bigO/02_complexidade_0_1.py
@@ -0,0 +1,14 @@
+
+def exemple_1():
+  return 'Hello World'
+
+def exemple_2(n):
+  return n
+
+def exemple_3(data):
+  return data[0]
+
+def exemplo_4():
+  queue = [] 
+  my_queue = queue.Queue()
+  my_queue.put(1)
